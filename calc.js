d560774f981f07c72f15e1b0f06957019614153a
Bradley Griffin <bigriff11@gmail.com>
2022-12-01 11:59:56 -0700

    Add working calculator

--- calc.js
+++ calc.js
@@ -9,6 +9,9 @@ function multiply(a, b){
     return a * b;
 }
 function divide(a, b){
+    if (b == 0) {
+        return "Nalgas"
+    }
     return a / b;
 }
 
@@ -25,5 +28,63 @@ function operate(operator, a, b){
     if (operator === "/"){
         return divide(a, b)
     }
-    else return `${a} + ${b}`;
+    else return a;
+}
+
+let screenDiv = document.querySelector('.screen')
+let operators = document.querySelectorAll('.operator')
+let numbers = document.querySelectorAll(".number")
+let clear = document.querySelector(".clear")
+let equal = document.querySelector(".enter")
+
+let firstNumber = ""
+let secondNumber = ""
+let op = ''
+let isComplete = false
+
+
+numbers.forEach(number => {
+    number.addEventListener('click', setNumber)
+})
+operators.forEach(item => {
+    item.addEventListener('click', setOperand)
+})
+clear.addEventListener('click', reset)
+equal.addEventListener('click', run)
+
+function update() {
+    let screen = `${firstNumber} ${op} ${secondNumber}`
+    screenDiv.textContent = screen
+    console.log(firstNumber.length)
+}
+
+function setNumber(){
+    if (!isComplete){
+        firstNumber += this.textContent;
+    }
+    if (isComplete){
+        secondNumber += this.textContent;
+    }
+    update()
+}
+
+function setOperand() {
+    op = this.textContent;
+    isComplete = true;
+    update()
+}
+
+function reset() {
+    firstNumber = '';
+    secondNumber = '';
+    op = '';
+    isComplete = false;
+    update()
+}
+
+function run(){
+    let result = operate(op, parseInt(firstNumber), parseInt(secondNumber));
+    reset()
+    firstNumber = result
+    update()
 }
